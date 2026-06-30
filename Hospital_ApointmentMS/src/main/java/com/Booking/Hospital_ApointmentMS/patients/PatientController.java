a7033e0b25759be2161fb20829d25df9fe581858
HP <ladiif95@gmail.com>
2026-06-30 10:28:19 -0700

    added update and delet methods

--- Hospital_ApointmentMS/src/main/java/com/Booking/Hospital_ApointmentMS/patients/PatientController.java
+++ Hospital_ApointmentMS/src/main/java/com/Booking/Hospital_ApointmentMS/patients/PatientController.java
@@ -1,23 +1,39 @@
-package com.Booking.Hospital_ApointmentMS.Patients;
+package com.Booking.Hospital_ApointmentMS.patients;
 
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/patients")
 @RequiredArgsConstructor
+@RequestMapping("api/patients")
 public class PatientController {
 
     private final PatientService service;
 
+    @PostMapping
+    public void SavePatient(@RequestBody Patient patient) {
+        service.addPatient(patient);
+    }
+
     @GetMapping
-    public List<Patient> GetAllPatient(){
+    public List<Patient> GetAllPatient() {
         return service.GetAllPatient();
     }
 
+    @GetMapping("/{id}")
+    public Patient getById(@PathVariable Long id) {
+        return service.getPatientById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Patient updatePatient(@PathVariable Long id, @RequestBody Patient patient) {
+        return service.updatePatient(id, patient);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePatient(@PathVariable Long id) {
+        service.deletePatient(id);
+    }
 }
--- Hospital_ApointmentMS/src/main/java/com/Booking/Hospital_ApointmentMS/patients/PatientRepository.java
+++ Hospital_ApointmentMS/src/main/java/com/Booking/Hospital_ApointmentMS/patients/PatientRepository.java
@@ -1,4 +1,4 @@
-package com.Booking.Hospital_ApointmentMS.Patients;
+package com.Booking.Hospital_ApointmentMS.patients;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
